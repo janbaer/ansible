6de40462463e451ab043d7d2258cef60f5cd51a4
Jan Baer <jan@janbaer.de>
2021-02-24 20:20:37 +0100

    Migrate all playbooks to new layout

---
+++ run-playbook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Install some preconditions..."
+  pamac install --no-confirm ansible yay-bin ansible-aur-git
+fi
+
+HOST=$(cat /etc/hostname)
+ANSIBLE_STDOUT_CALLBACK=yaml
+
+echo "Run playbook playbook.yml on machine ${HOST}"
+
+ansible-playbook playbook.yml --limit=${HOST} --ask-become-pass --ask-vault-pass "$@"
