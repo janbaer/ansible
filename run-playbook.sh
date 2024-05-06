040b4eb391f0c2a456d14b9355c4aec5f1030bcc
Jan Baer <jan@janbaer.de>
2024-05-06 22:21:54 +0200

    Read passwords from Gopass to prevent entering the passwords all the time

--- run-playbook.sh
+++ run-playbook.sh
@@ -2,6 +2,13 @@
 
 export ANSIBLE_STDOUT_CALLBACK="yaml"
 
+function cleanup {
+  echo "Cleaning up..."
+  rm -r ./.vault_pass
+}
+
+trap cleanup EXIT
+
 if [ $# -eq 0 ]; then
   echo "Install some preconditions..."
   pamac install --no-confirm ansible yay fakeroot ansible-aur
@@ -11,4 +18,22 @@ HOST=$(tr '[:upper:]' '[:lower:]' < /etc/hostname)
 
 echo "Run playbook playbook.yml on machine ${HOST}"
 
-ansible-playbook playbook.yml -e @group_vars/vault.yml --limit="${HOST}" --ask-become-pass --ask-vault-pass --diff "$@"
+if command -v gopass &> /dev/null; then
+  echo "Read passwords from gopass"
+  become_pass=$(gopass /ansible/become_password)
+  vault_pass=$(gopass /ansible/vault_password)
+fi
+
+if [ -n "${become_pass}" ] && [ -n "${vault_pass}" ]; then
+  echo "Use passwords from gopass"
+  echo "$vault_pass" > .vault_pass
+  export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass
+
+  ansible-playbook playbook.yml \
+    -e @group_vars/vault.yml    \
+    -e ansible_become_pass="$become_pass"\
+    -e ansible_vault_pass="$vault_pass"\
+    --limit="${HOST}" --diff "$@"
+else
+  ansible-playbook playbook.yml -e @group_vars/vault.yml --limit="${HOST}" --ask-become-pass --ask-vault-pass --diff "$@"
+fi
